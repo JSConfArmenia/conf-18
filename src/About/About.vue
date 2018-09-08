4f8eda50b8f0cba7cd005b63b61a1b7fd557754d
Gevorg Harutyunyan <gevharut@gmail.com>
2018-09-09 02:35:44 +0400

    About section

--- src/About/About.vue
+++ src/About/About.vue
@@ -5,8 +5,18 @@
         <div class="col col-md-3 offset-md-1">
           <Ninja />
         </div>
-        <div class="col col-md-8">
+        <div class="col col-md-7 offset-md-1">
+          <h1 class="Title">What Is</h1>
+          <h3 class="Subtitle">JavaScript Conference Armenia?</h3>
 
+          <h4 class="Answer">Where JavaScript Ninjas meet!</h4>
+
+          <p class="Description">
+            JavaScript Conference provides a platform for developers to share their ideas,
+            experiences and knowledge. Come and learn about the modern
+            development ecosystem by attending talks and workshops.
+            Network with other developers who love what they do.
+          </p>
         </div>
       </div>
     </div>
@@ -24,5 +34,27 @@ export default {
 </script>
 
 <style scoped>
+.About {
+  background-image: linear-gradient(to bottom, #fff, #f5f5f5);
+}
 
+.Title {
+  font-size: 75px;
+  text-transform: uppercase;
+  color: #ffdd00;
+  margin-bottom: 0;
+  line-height: 1em;
+}
+
+.Subtitle {
+  font-size: 28px;
+  line-height: 1em;
+  margin-bottom: 1em;
+  text-transform: uppercase;
+}
+
+.Answer {
+  font-size: 24px;
+  margin-bottom: 1em;
+}
 </style>
